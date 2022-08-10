6654f505ce74a79589959c28c301ce81ca8a03c1
Linda-Andoh <lexicon736@gmail.com>
2022-08-10 18:55:29 -0400

    "created timmys story"

--- homework.js
+++ homework.js
@@ -26,7 +26,7 @@ class Hamster {
   Gus.getPrice();
 
 
-class Person {
+class Person{
     constructor(name, age, height, weight, mood, hamsters, bankAccount) {
         this.name = name;
         this.age = 0;
@@ -46,54 +46,6 @@ class Person {
         console.log(`${this.age}`);
         return this.age;
     }
-
-    getWeight() {
-        console.log(`${this.weight}`);
-        return this.weight;
-    }
-
-    greet(name) {
-        console.log(`Hello, ${name}!`);
-    }
-
-    eat() {
-        this.weight++;
-        this.mood += 1;
-    }
-
-    exercise() {
-        this.weight--;
-    }
-
-    ageUp() {
-        this.age += 15;
-        this.height += 1;
-        this.weight += 2;
-        this.mood -= 1;
-        this.bankAccount += 10;
-    }
-
-    buyHamster(hamster) {
-        this.hamsters.push(hamster);
-        this.mood += 10;
-        this.bankAccount -= hamster.getPrice(hamster);
-    }
 }
-
-const timmy = new Person('Timmy');
-console.log(timmy);
-
-timmy.eat();
-console.log(timmy);
-
-timmy.ageUp();
-console.log(timmy.age);
-console.log(timmy);
-
-timmy.exercise();
-console.log(timmy);
-
-timmy.buyHamster(Gus);
-console.log(timmy);
 
 
